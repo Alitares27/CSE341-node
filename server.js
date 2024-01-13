311f556589efab19cf8e74529fd693988fe6004c
Alitares27 <aldair2795@hotmail.com>
2024-01-13 13:57:06 -0300

    connecting to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mongodb = require('./database');
+
 const port = 8080;
 
 app.use(express.static('frontend'));
 
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || port));
-});
+mongodb.initDb((err)=> {
+  if(err){
+    console.log(err)
+  } else{
+
+    app.listen(process.env.PORT || port, () => {
+      console.log('Web Server is listening at port ' + (process.env.PORT || port));
+    });
+
+  }});
