35a892c713dc47b12d3b2124ed8e1bfd7cd38c84
Alitares27 <aldair2795@hotmail.com>
2024-01-14 00:12:17 -0300

    Add the Get and GetAll endpoints

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
-const mongodb = require('./database');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./data/database');
+const userRoutes = require('./routes/users');
 
 const port = 8080;
 
 app.use(express.static('frontend'));
 
-mongodb.initDb((err)=> {
-  if(err){
-    console.log(err)
-  } else{
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        })
+        .use('/users', userRoutes);
 
-    app.listen(process.env.PORT || port, () => {
-      console.log('Web Server is listening at port ' + (process.env.PORT || port));
-    });
 
-  }});
+      mongodb.initDb((err, mongodb) => {
+        if (err) {
+          console.log(err)
+        } else {
+
+          app.listen(process.env.PORT || port, () => {
+            console.log('Web Server is listening at port ' + (process.env.PORT || port));
+          });
+
+        }
+      });
