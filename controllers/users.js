935e3af61cf773919239360edd9cdf9311557f18
Alitares27 <aldair2795@hotmail.com>
2024-01-17 22:57:16 -0300

    updating code

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,14 @@
 const mongodb = require('../data/database');
 const objectId = require('mongodb').objectId;
 
-const getAll = async (req, res,next ) => {
+const getAll = async (req, res ) => {
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     })
 };
-const getSingle = async (req, res, next) => {
+const getSingle = async (req, res) => {
     const userId = new objectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({
         _id: userId
@@ -22,4 +22,4 @@ module.exports = {
     getAll,
     getSingle,
 
-}
+};
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./data/database');
+
 const userRoutes = require('./routes/users');
 
 const port = 8080;
+
+app.use('/', require('./routes/'));
 
 app.use(express.static('frontend'));
 
@@ -24,7 +27,7 @@ app
         } else {
 
           app.listen(process.env.PORT || port, () => {
-            console.log('Web Server is listening at port ' + (process.env.PORT || port));
+            console.log('Database is listening and Node is listening on port ' + (process.env.PORT || port));
           });
 
         }
